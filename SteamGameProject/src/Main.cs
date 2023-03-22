9bdc5e1af1cdffb162dfce1ad4e49263a4d2a97d
Joanna May <joannamadisonmay@gmail.com>
2023-03-22 09:15:42 -0500

    test: try resolve assemblies

--- SteamGameProject/src/Main.cs
+++ SteamGameProject/src/Main.cs
@@ -4,9 +4,31 @@ using Godot;
 #if DEBUG
 using System.Reflection;
 using GoDotTest;
+using System.Runtime.InteropServices;
+using System.IO;
+using System;
+using System.Runtime.CompilerServices;
 #endif
 
 public partial class Main : Node2D {
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  public Main() {
+    NativeLibrary.Load(
+      Path.Join(AppContext.BaseDirectory, "libsteam_api.dylib")
+    );
+    AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblies;
+  }
+
+  private Assembly? ResolveAssemblies(object? sender, ResolveEventArgs args) {
+    // Never gets called :'(
+    var assemblyName = new AssemblyName(args.Name);
+    var assemblyPath = Path.Join(
+      AppContext.BaseDirectory,
+      assemblyName.Name + ".dll"
+    );
+    return File.Exists(assemblyPath) ? Assembly.LoadFrom(assemblyPath) : null;
+  }
+
   public TestEnvironment Environment = default!;
 
   public override void _Ready() {
